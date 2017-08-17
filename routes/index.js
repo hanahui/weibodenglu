f7b583746be745b4627a0702b87a94d6743bdbea
hanahui <1017672884@qq.com>
2017-08-17 09:34:10 +0800

    配置环境设置路由

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,44 @@
-var express = require('express');
-var router = express.Router();
+// var express = require('express');
+// var router = express.Router();
+//
+// /* GET home page. */
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: '首页' });
+// });
+//
+// module.exports = router;
+module.exports = function(app){
+    //首页路由
+    app.get("/",function(req,res){
+       res.render('index',{title:'首页'});
+    })
+    //注册页面
+    app.get("/reg",function(req,res){
+        res.render("index",{title:"注册"});
+    })
+    //注册行为
+    app.post("/reg",function(req,res){
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+    })
+//    登录页面
+    app.get("/login",function(req,res){
+        res.render("login",{
+            title:"登录"
+        })
+    })
+    //登录行为
+    app.post("/longin",function(req,res){
 
-module.exports = router;
+    })
+    app.get("/post",function(req,res){
+        res.render("post",{
+            title:"发布文章"
+        })
+    })
+    app.post("/post",function(req,res){
+
+    })
+    app.get("/logout",function(req,res){
+
+    })
+}
