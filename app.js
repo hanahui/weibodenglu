1ce9c3b4e021cfe1f55e2d9e7de514bdd925e822
hanahui <1017672884@qq.com>
2017-08-17 10:26:15 +0800

    配置数据库完成

--- app.js
+++ app.js
@@ -4,15 +4,23 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
-var index = require('./routes/index');
-var users = require('./routes/users');
+// var db = require("./modules/db");
+var routes = require('./routes/index');
+// var users = require('./routes/users');
+//引入数据库配置文件
+var setting = require("./setting");
+//临时存放一些数据
+var flash = require("connect-flash");
+//z支持会话
+var session = require("express-session");
+//把回话白存在MongoDB中区
+var MongoStore = require("connect-mongo")(session);
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -22,9 +30,23 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/users', users);
+//使用flash
+app.use(flash());
+//
+app.use(session({
+    secret:"hahBlog",
+    key:setting.db,
+    cookie:{maxAge:30*24*60*60*1000},
+    store:new MongoStore({
+        url: 'mongodb://localhost/hanahui'
+    }),
+    resave:false,
+    saveUninitialized:true
+}))
 
+// app.use('/', index);
+// app.use('/users', users);
+routes(app);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
@@ -37,10 +59,11 @@ app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
   // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
-
+app.listen(3000,function(){
+  console.log("node is ok!")
+})
 module.exports = app;
